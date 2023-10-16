acfc0e8a76d721615bf9fd5b34a108dcd94ba75e
ardacey <ac.ardaceylan@gmail.com>
2023-10-16 17:40:18 +0300

    fetch update

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,6 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-import { getCurrentWeatherData, getForecastWeatherData} from "@/services/weather";
-
 interface ForecastData {
   dt: number;
   dt_txt: string;
@@ -26,7 +24,25 @@ interface ForecastData {
 
 export default async function Home() {
 
-    const [currentWeatherData, forecastWeatherData] = await Promise.all([
+  const apiKey = process.env.PUBLIC_WEATHER_API_KEY;
+  const apiUrl = 'https://api.openweathermap.org/data/2.5';
+
+  const makeWeatherApiCall = async (endpoint: string) => {
+      const response = await fetch(`${apiUrl}${endpoint}&appid=${apiKey}&units=metric`, { next: { revalidate: 10 } });
+      return response.json();
+    };
+    
+  const getCurrentWeatherData = async () => {
+      const currentWeatherData = await makeWeatherApiCall(`/weather?lat=39.8674482&lon=32.7353845`);
+      return currentWeatherData;
+    };
+    
+  const getForecastWeatherData = async () => {
+      const forecastWeatherData = await makeWeatherApiCall(`/forecast?lat=39.8674482&lon=32.7353845`);
+      return forecastWeatherData;
+    };
+  
+  const [currentWeatherData, forecastWeatherData] = await Promise.all([
     getCurrentWeatherData(),
     getForecastWeatherData(),
   ]);
